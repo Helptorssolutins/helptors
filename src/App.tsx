1d1532ce092d84410537a633cf51f4eb1df3e60b
daveclinton <clintondavid46@gmail.com>
2022-09-17 10:59:14 +0300

    feature: Add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import SidebarWithNavbar from "./components/navbar";
 
-export const App = () => <ChakraProvider theme={theme}></ChakraProvider>;
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <SidebarWithNavbar children={undefined} />
+  </ChakraProvider>
+);
