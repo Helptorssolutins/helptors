ea569d56741b46fa79cdb5187ed0daba48979063
daveclinton <clintondavid46@gmail.com>
2022-09-17 10:28:08 +0300

    fix: Remove unused Color Strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
-import { Box, ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 
 export const App = () => <ChakraProvider theme={theme}></ChakraProvider>;
